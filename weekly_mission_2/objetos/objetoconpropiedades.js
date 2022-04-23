874b67540e0a5db680938f7590a842391fa9ef89
Gerardo Juarez Salmoran <gerardo_juarez@hotmail.com>
2022-04-23 17:55:51 -0500

    se agregaron los ejercicios

---
+++ weekly_mission_2/objetos/objetoconpropiedades.js
@@ -0,0 +1,7 @@
+//Ejemplo 2 aqui creo un objeto con propiedades
+const myObject2 = {
+    name: "Gerardo Juarez",
+    age: 31
+}
+console.log("Ejemplo 2 este es un objeto con propiedades")
+console.log(myObject2)
---
+++ weekly_mission_2/objetos/objetovacio.js
@@ -0,0 +1,4 @@
+// ejemplo 1 Creacion de un objeto vacio
+const myObject = {} // aqui se crea un objeto vacio
+console.log("Ejemplo 1: crear un objeto vacio")
+console.log(myObject)
